bc8b7e08e7cdc7fe7028a6fe9aed66625bcf5245
Howling Techie <howling.techie@gmail.com>
2024-02-28 10:47:56 +0000

    Add data wiping warning and abstract button functionality.
    The commit involves abstracting the button functionality in Item.jsx and introducing a warning for items that require data wiping. Added non-functional file upload button.

--- src/routes/Processing/Item.jsx
+++ src/routes/Processing/Item.jsx
@@ -1,8 +1,17 @@
 import {Container} from "../../components/Container.jsx";
 import {Header} from "../../components/Header.jsx";
-import {FaBan, FaCheck, FaHardDrive, FaRegClock, FaTriangleExclamation, FaVanShuttle} from "react-icons/fa6";
+import {
+    FaBan,
+    FaCheck, FaFile,
+    FaHardDrive,
+    FaRegClock,
+    FaTriangleExclamation,
+    FaVanShuttle
+} from "react-icons/fa6";
 import {useParams} from "react-router-dom";
 import {ItemNotes} from "../../components/ItemNotes.jsx";
+import {Fragment, useRef, useState} from 'react'
+import {Dialog, Transition} from '@headlessui/react'
 
 function classNames(...classes) {
     return classes.filter(Boolean).join(' ')
@@ -12,6 +21,7 @@ const ActionButton = ({type, text, callback}) => (
     <button
         type="button"
         disabled={type === "disabled"}
+        onClick={callback}
         className={classNames(type === "danger" ? "bg-red-700 hover:bg-red-600" :
             type === "disabled" ? "bg-neutral-600" :
                 "bg-indigo-500 hover:bg-indigo-400", " rounded-md px-2.5 py-1.5 text-sm font-semibold text-white shadow-sm focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500")}
@@ -144,8 +154,8 @@ const testItems = [
     {
         "id": 6,
         "reference_number": "REF006",
-        "name": "Mouse",
-        "original_name": null,
+        "name": "Hard Drive",
+        "original_name": "Power Bank",
         "transfer": {
             "id": 106,
             "reference_number": "TRANS106",
@@ -153,12 +163,17 @@ const testItems = [
             "collection_date": "2024-03-04"
         },
         "status": "collected",
-        "requires_wiping": false,
+        "requires_wiping": true,
         "notes": [
             {
                 "date_added": "2024-03-02",
                 "added_by": "Admin",
                 "note": "Item approved."
+            },
+            {
+                "date_added": "2024-03-04",
+                "added_by": "Admin",
+                "note": "Item renamed as not as described."
             },
             {
                 "date_added": "2024-03-04",
@@ -274,6 +289,9 @@ const testItems = [
 ];
 export const Item = () => {
     const {transferId, itemId} = useParams();
+    const [open, setOpen] = useState(false)
+
+    const cancelButtonRef = useRef(null)
 
     const item = testItems[itemId];
     const transfer = item.transfer;
@@ -301,6 +319,76 @@ export const Item = () => {
 
     return (
         <Container>
+            <Transition.Root show={open} as={Fragment}>
+                <Dialog as="div" className="relative z-10" initialFocus={cancelButtonRef}
+                        onClose={setOpen}>
+                    <Transition.Child
+                        as={Fragment}
+                        enter="ease-out duration-300"
+                        enterFrom="opacity-0"
+                        enterTo="opacity-100"
+                        leave="ease-in duration-200"
+                        leaveFrom="opacity-100"
+                        leaveTo="opacity-0"
+                    >
+                        <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity"/>
+                    </Transition.Child>
+
+                    <div className="fixed inset-0 z-10 overflow-y-auto">
+                        <div
+                            className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
+                            <Transition.Child
+                                as={Fragment}
+                                enter="ease-out duration-300"
+                                enterFrom="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+                                enterTo="opacity-100 translate-y-0 sm:scale-100"
+                                leave="ease-in duration-200"
+                                leaveFrom="opacity-100 translate-y-0 sm:scale-100"
+                                leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+                            >
+                                <Dialog.Panel
+                                    className="relative transform overflow-hidden rounded-lg bg-white px-4 pb-4 pt-5 text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg sm:p-6">
+                                    <div>
+                                        <div
+                                            className="mx-auto flex h-12 w-12 items-center justify-center rounded-full bg-green-100">
+                                            <FaCheck className="h-6 w-6 text-green-600" aria-hidden="true"/>
+                                        </div>
+                                        <div className="mt-3 text-center sm:mt-5">
+                                            <Dialog.Title as="h3"
+                                                          className="text-base font-semibold leading-6 text-gray-900">
+                                                Confirm Processing
+                                            </Dialog.Title>
+                                            <div className="mt-2">
+                                                <p className="text-sm text-gray-500">
+                                                    This item requires data wiping, has this been done?
+                                                </p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div
+                                        className="mt-5 sm:mt-6 sm:grid sm:grid-flow-row-dense sm:grid-cols-2 sm:gap-3">
+                                        <button
+                                            type="button"
+                                            className="inline-flex w-full justify-center rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 sm:col-start-2"
+                                            onClick={() => setOpen(false)}
+                                        >
+                                            Process
+                                        </button>
+                                        <button
+                                            type="button"
+                                            className="mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:col-start-1 sm:mt-0"
+                                            onClick={() => setOpen(false)}
+                                            ref={cancelButtonRef}
+                                        >
+                                            Cancel
+                                        </button>
+                                    </div>
+                                </Dialog.Panel>
+                            </Transition.Child>
+                        </div>
+                    </div>
+                </Dialog>
+            </Transition.Root>
             <Header path={[{link: "/processing", title: "Processing"},
                 {link: "/processing/transfers", title: "Transfers"},
                 {
@@ -318,10 +406,14 @@ export const Item = () => {
                         <div className="font-semibold pb-8">
                         <span
                             className="text-neutral-400 font-display">Received on </span>{new Date(2024, 1, 23).toDateString()}
-                        </div>
-                        <div className="font-semibold pt-8">
+                            {item.original_name && <div className="font-semibold pt-3">
                         <span
-                            className="text-neutral-400 font-display">Status: </span>{item.status}
+                            className="text-neutral-400 font-display">Originally named </span>{item.original_name}
+                            </div>}
+                        </div>
+                        <div className="font-semibold py-8">
+                        <span
+                            className="text-neutral-400 font-display">Status: </span>{item.status.at(0).toUpperCase() + item.status.slice((1))}
                             <div className="pt-4 flex space-x-2">
                                 {item.status === "denied" ? <>
                                     <ActionButton type="danger" text="Mark as Approved"/>
@@ -346,7 +438,9 @@ export const Item = () => {
                                             Requires Wiping
                                         </div>
                                     </div>
-                                    <ActionButton text="Mark as Processed"/>
+                                    <ActionButton text="Mark as Processed" callback={() => {
+                                        setOpen(true);
+                                    }}/>
                                 </> : item.status === "processed" ? <>
                                     <ActionButton type="disabled" text="Mark as Denied"/>
                                     <ActionButton type="disabled" text="Mark as Uncollected"/>
@@ -364,7 +458,7 @@ export const Item = () => {
                                     </div>
                                     <ActionButton type="danger" text="Mark as Unprocessed"/>
                                 </> : item.status === "skipped" ? <>
-                                    <ActionButton type="disabled" text="Mark as Denied"/>
+                                    <ActionButton type="danger" text="Mark as Denied"/>
                                     <ActionButton text="Mark as Collected"/>
                                     <ActionButton type="disabled" text="Mark as Processed"/>
                                 </> : <>
@@ -375,6 +469,45 @@ export const Item = () => {
                                 }
                             </div>
                         </div>
+                        {item.requires_wiping && <div className="pt-8">
+                            <div>
+                                <label htmlFor="email" className="block text-sm font-medium leading-6 text-neutral-100">
+                                    Hard Drive Serial Number
+                                </label>
+                                <div className="mt-2 flex-row flex space-x-2">
+                                    <input
+                                        type="serial"
+                                        name="serial"
+                                        id="serial"
+                                        className="grow rounded-md py-1.5 text-neutral-100 ring-1 ring-inset ring-gray-300 placeholder:text-neutral-500 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 bg-neutral-900 pl-2"
+                                        placeholder="123456789"
+                                    />
+                                    <ActionButton text="Save"/>
+                                </div>
+                            </div>
+                            <div className="pt-4">
+                                <label htmlFor="data-cert"
+                                       className="block text-sm font-medium leading-6 text-neutral-100">
+                                    Data Certificate
+                                </label>
+                                <div
+                                    className="mt-2 flex justify-center rounded-lg border border-dashed border-neutral-100/25 px-6 py-10">
+                                    <div className="text-center">
+                                        <FaFile className="mx-auto h-12 w-12 text-gray-300" aria-hidden="true"/>
+                                        <div className="mt-4 flex text-sm justify-center">
+                                            <label
+                                                htmlFor="file-upload"
+                                                className="relative cursor-pointer rounded-md bg-neutral-900 font-semibold text-neutral-100 focus-within:outline-none focus-within:ring-2 focus-within:ring-indigo-600 focus-within:ring-offset-2 hover:text-indigo-400 p-4"
+                                            >
+                                                <span>Upload a certificate</span>
+                                                <input id="file-upload" name="file-upload" type="file"
+                                                       className="sr-only"/>
+                                            </label>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>}
                     </div>
                 </div>
                 <div className="w-full md:w-1/3 flex flex-col space-y-2">
